fe3008b92d92197d1bead3ee6a4cf24f721d71e4
Muhammad Mazhar Qayyum <mmazhar554@gmail.com>
2023-12-21 21:41:10 +0500

    Remove .js files from version control

--- Q9.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your 
-//favorite number. Print that message.
-let favnum = 1;
-let mxg = "My favorite number is";
-console.log(`${mxg} ${favnum}`);
